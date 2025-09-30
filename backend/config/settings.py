c9a253b60304753263d8217de3f4c69562b9e293
Admin <deployer@info101.example.com>
2025-09-30 20:42:29 +0300

    Настройка запуска сервера

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 
 # Static / Media
 STATIC_URL = "static/"
+STATIC_ROOT = os.getenv("STATIC_ROOT", str(BASE_DIR / "staticfiles"))
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # === Recordings & media serving (DEV) ===
